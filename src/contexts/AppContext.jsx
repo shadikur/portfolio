f615cb5ea1bc52a3da61d8a82f38863a0331f080
Mohammad Rahman <me@shadikur.com>
2023-07-01 00:13:48 +0200

    Router setup

---
+++ src/contexts/AppContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext } from 'react';
+
+export const myContext = createContext(null)
+
+const conextProvider = () => {
+
+}
+
+const AppContext = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default AppContext;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const routes = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App></App>,
+      errorElement: <p>Error!</p>
+    }
+  ]
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <RouterProvider router={routes}>
     <App />
-  </React.StrictMode>,
+  </RouterProvider>,
 )
