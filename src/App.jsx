26699b11b90873c98cae6c2604473b12b469c16b
Mohammad Rahman <me@shadikur.com>
2023-07-01 11:17:19 +0200

    MUI Installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Banner from './components/Banner/Banner';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Portfolio from './components/Portfolio/Portfolio';
 
 const App = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <Header></Header>
+            <Banner></Banner>
+            <About></About>
+            <Portfolio></Portfolio>
+            <Contact></Contact>
+            <Footer></Footer>
+        </>
     );
 };
 
