83753118ad95618c2996edc0cbf00b5496e21d30
Mohammad Rahman <me@shadikur.com>
2023-07-15 00:15:44 +0200

    Reload with Login updated

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Logo = () => {
     return (
-        <img src='https://res.cloudinary.com/ddez9nchs/image/upload/v1631235242/shadikur/shadikur_logo_300_125_dark.png' alt='Shadikur Logo' className='w-40'>
-        </img>
+        <a href="/" alt="Shadikur - Best use of Linux">
+            <img src='https://res.cloudinary.com/ddez9nchs/image/upload/v1631235242/shadikur/shadikur_logo_300_125_dark.png' alt='Shadikur Logo' className='w-40'>
+            </img>
+        </a>
     );
 };
 
